a329002f488fa1c0fbde987816e604439c0ec717
Pereira Ricardo <r.pereira@madeinitalyslc.it>
2019-03-11 18:14:14 +0100

    Work update

--- src/AbstractPlugin.php
+++ src/AbstractPlugin.php
@@ -24,185 +24,185 @@ if (\class_exists('AbstractPlugin')) return;
  */
 abstract class AbstractPlugin extends Container implements PluginInterface, ContainerInterface
 {
-	/**
-	 * Plugin basename.
-	 *
-	 * Ex: plugin-name/plugin-name.php
-	 *
-	 * @var string
-	 */
-	protected $basename;
+    /**
+     * Plugin basename.
+     *
+     * Ex: plugin-name/plugin-name.php
+     *
+     * @var string
+     */
+    protected $basename;
 
-	/**
-	 * Absolute path to the main plugin directory.
-	 *
-	 * @var string
-	 */
-	protected $directory;
+    /**
+     * Absolute path to the main plugin directory.
+     *
+     * @var string
+     */
+    protected $directory;
 
-	/**
-	 * Absolute path to the main plugin file.
-	 *
-	 * @var string
-	 */
-	protected $file;
+    /**
+     * Absolute path to the main plugin file.
+     *
+     * @var string
+     */
+    protected $file;
 
-	/**
-	 * Plugin identifier.
-	 *
-	 * @var string
-	 */
-	protected $slug;
+    /**
+     * Plugin identifier.
+     *
+     * @var string
+     */
+    protected $slug;
 
-	/**
-	 * URL to the main plugin directory.
-	 *
-	 * @var string
-	 */
-	protected $url;
-    
+    /**
+     * URL to the main plugin directory.
+     *
+     * @var string
+     */
+    protected $url;
+
     /**
      * Version of the plugin
      *
      * @var string
      */
-	protected $version;
+    protected $version;
 
-	/**
-	 * Retrieve the absolute path for the main plugin file.
-	 *
-	 * @return string
-	 */
-	public function getBasename()
-	{
-		return $this->basename;
-	}
+    /**
+     * Retrieve the absolute path for the main plugin file.
+     *
+     * @return string
+     */
+    public function getBasename()
+    {
+        return $this->basename;
+    }
 
-	/**
-	 * Set the plugin basename.
-	 *
-	 * @param  string $basename Relative path from the main plugin directory.
-	 * @return $this
-	 */
-	public function setBasename($basename)
-	{
-		$this->basename = $basename;
+    /**
+     * Set the plugin basename.
+     *
+     * @param  string $basename Relative path from the main plugin directory.
+     * @return $this
+     */
+    public function setBasename($basename)
+    {
+        $this->basename = $basename;
 
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	 * Retrieve the plugin directory.
-	 *
-	 * @return string
-	 */
-	public function getDirectory()
-	{
-		return $this->directory;
-	}
+    /**
+     * Retrieve the plugin directory.
+     *
+     * @return string
+     */
+    public function getDirectory()
+    {
+        return $this->directory;
+    }
 
-	/**
-	 * Set the plugin's directory.
-	 *
-	 * @param  string $directory Absolute path to the main plugin directory.
-	 * @return $this
-	 */
-	public function setDirectory($directory)
-	{
-		$this->directory = rtrim($directory, '/') . '/';
+    /**
+     * Set the plugin's directory.
+     *
+     * @param  string $directory Absolute path to the main plugin directory.
+     * @return $this
+     */
+    public function setDirectory($directory)
+    {
+        $this->directory = rtrim($directory, '/') . '/';
 
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	 * Retrieve the path to a file in the plugin.
-	 *
-	 * @param  string $path Optional. Path relative to the plugin root.
-	 * @return string
-	 */
-	public function getPath($path = '')
-	{
-		return $this->directory . ltrim($path, '/');
-	}
+    /**
+     * Retrieve the path to a file in the plugin.
+     *
+     * @param  string $path Optional. Path relative to the plugin root.
+     * @return string
+     */
+    public function getPath($path = '')
+    {
+        return $this->directory . ltrim($path, '/');
+    }
 
-	/**
-	 * Retrieve the absolute path for the main plugin file.
-	 *
-	 * @return string
-	 */
-	public function getFile()
-	{
-		return $this->file;
-	}
+    /**
+     * Retrieve the absolute path for the main plugin file.
+     *
+     * @return string
+     */
+    public function getFile()
+    {
+        return $this->file;
+    }
 
-	/**
-	 * Set the path to the main plugin file.
-	 *
-	 * @param  string $file Absolute path to the main plugin file.
-	 * @return $this
-	 */
-	public function setFile($file)
-	{
-		$this->file = $file;
+    /**
+     * Set the path to the main plugin file.
+     *
+     * @param  string $file Absolute path to the main plugin file.
+     * @return $this
+     */
+    public function setFile($file)
+    {
+        $this->file = $file;
 
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	 * Retrieve the plugin identifier.
-	 *
-	 * @return string
-	 */
-	public function getSlug()
-	{
-		return $this->slug;
-	}
+    /**
+     * Retrieve the plugin identifier.
+     *
+     * @return string
+     */
+    public function getSlug()
+    {
+        return $this->slug;
+    }
 
-	/**
-	 * Set the plugin identifier.
-	 *
-	 * @param  string $slug Plugin identifier.
-	 * @return $this
-	 */
-	public function setSlug($slug)
-	{
-		$this->slug = $slug;
+    /**
+     * Set the plugin identifier.
+     *
+     * @param  string $slug Plugin identifier.
+     * @return $this
+     */
+    public function setSlug($slug)
+    {
+        $this->slug = $slug;
 
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	 * Retrieve the URL for a file in the plugin.
-	 *
-	 * @param  string $path Optional. Path relative to the plugin root.
-	 * @return string
-	 */
-	public function getUrl($path = '')
-	{
-		return $this->url . ltrim($path, '/');
-	}
+    /**
+     * Retrieve the URL for a file in the plugin.
+     *
+     * @param  string $path Optional. Path relative to the plugin root.
+     * @return string
+     */
+    public function getUrl($path = '')
+    {
+        return $this->url . ltrim($path, '/');
+    }
 
-	/**
-	 * Set the URL for plugin directory root.
-	 *
-	 * @param  string $url URL to the root of the plugin directory.
-	 * @return $this
-	 */
-	public function setUrl($url)
-	{
-		$this->url = rtrim($url, '/') . '/';
+    /**
+     * Set the URL for plugin directory root.
+     *
+     * @param  string $url URL to the root of the plugin directory.
+     * @return $this
+     */
+    public function setUrl($url)
+    {
+        $this->url = rtrim($url, '/') . '/';
 
-		return $this;
-	}
-    
+        return $this;
+    }
+
     /**
      * @return string
      */
-	public function getVersion()
+    public function getVersion()
     {
         return $this->version;
     }
-    
+
     /**
      * @param string $version
      * @return $this
@@ -210,26 +210,26 @@ abstract class AbstractPlugin extends Container implements PluginInterface, Cont
     public function setVersion(string $version)
     {
         $this->version = $version;
-        
+
         return $this;
     }
 
-	/**
-	 * Register a hook provider.
-	 *
-	 * @param  HookProviderInterface $provider Hook provider.
-	 * @return $this
-	 */
-	public function registerHooks(HookProviderInterface $provider)
-	{
-		if ($provider instanceof ContainerAwareInterface) {
-			$provider->setContainer($this);
-		}
+    /**
+     * Register a hook provider.
+     *
+     * @param  HookProviderInterface $provider Hook provider.
+     * @return $this
+     */
+    public function registerHooks(HookProviderInterface $provider)
+    {
+        if ($provider instanceof ContainerAwareInterface) {
+            $provider->setContainer($this);
+        }
 
-		$provider->registerHooks();
+        $provider->registerHooks();
 
-		return $this;
-	}
+        return $this;
+    }
 
     /**
      * @param string $id
--- src/ContainerAwareTrait.php
+++ src/ContainerAwareTrait.php
@@ -23,78 +23,78 @@ if (\trait_exists('ContainerAwareTrait', true)) return;
  */
 trait ContainerAwareTrait
 {
-	/**
-	 * Container.
-	 *
-	 * @var ContainerInterface
-	 */
-	protected $container;
+    /**
+     * Container.
+     *
+     * @var ContainerInterface
+     */
+    protected $container;
 
-	/**
-	 * Proxy access to container services.
-	 *
-	 * @param  string $name Service name.
-	 * @return mixed
-	 */
-	public function __get($name)
-	{
-		return $this->container->get($name);
-	}
+    /**
+     * Proxy access to container services.
+     *
+     * @param  string $name Service name.
+     * @return mixed
+     */
+    public function __get($name)
+    {
+        return $this->container->get($name);
+    }
 
-	/**
-	 * Whether a container service exists.
-	 *
-	 * @param  string $name Service name.
-	 * @return bool
-	 */
-	public function __isset($name)
-	{
-		return $this->container->has($name);
-	}
+    /**
+     * Whether a container service exists.
+     *
+     * @param  string $name Service name.
+     * @return bool
+     */
+    public function __isset($name)
+    {
+        return $this->container->has($name);
+    }
 
-	/**
-	 * Calling a non-existant method on the class checks to see if there's an
-	 * item in the container that is callable and if so, calls it.
-	 *
-	 * @param  string $method Method name.
-	 * @param  array  $args   Method arguments.
-	 * @return mixed
-	 */
-	public function __call($method, $args)
-	{
-		if ($this->container->has($method)) {
-			$object = $this->container->get($method);
-			if (is_callable($object)) {
-				return call_user_func_array($object, $args);
-			}
-		}
-	}
+    /**
+     * Calling a non-existant method on the class checks to see if there's an
+     * item in the container that is callable and if so, calls it.
+     *
+     * @param  string $method Method name.
+     * @param  array  $args   Method arguments.
+     * @return mixed
+     */
+    public function __call($method, $args)
+    {
+        if ($this->container->has($method)) {
+            $object = $this->container->get($method);
+            if (is_callable($object)) {
+                return call_user_func_array($object, $args);
+            }
+        }
+    }
 
-	/**
-	 * Enable access to the DI container by plugin consumers.
-	 *
-	 * @return ContainerInterface
-	 */
-	public function getContainer()
-	{
-		return $this->container;
-	}
+    /**
+     * Enable access to the DI container by plugin consumers.
+     *
+     * @return ContainerInterface
+     */
+    public function getContainer()
+    {
+        return $this->container;
+    }
 
-	/**
-	 * Set the container.
-	 *
-	 * @param  ContainerInterface $container Dependency injection container.
-	 * @throws InvalidArgumentException when no container is provided that implements ContainerInterface.
-	 * @return $this
-	 */
-	public function setContainer($container)
-	{
-		if (!$container instanceof ContainerInterface) {
-			throw new \InvalidArgumentException('Expected a ContainerInterface.');
-		}
+    /**
+     * Set the container.
+     *
+     * @param  ContainerInterface $container Dependency injection container.
+     * @throws \InvalidArgumentException when no container is provided that implements ContainerInterface.
+     * @return $this
+     */
+    public function setContainer(ContainerInterface $container)
+    {
+        if (!$container instanceof ContainerInterface) {
+            throw new \InvalidArgumentException('Expected a ContainerInterface.');
+        }
 
-		$this->container = $container;
+        $this->container = $container;
 
-		return $this;
-	}
+        return $this;
+    }
 }
